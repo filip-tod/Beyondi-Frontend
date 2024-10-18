97c67c2880dd400db51d28b41eb60c730ffcd821
billybord <filip.tod95@gmail.com>
2024-10-18 16:25:14 +0200

    minor fix

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -4,7 +4,7 @@ import BurgerIcon from '../../assets/icons/NavBarMobile.svg';
 
 export const NavBar = () => {
 return(
-  <nav className="flex justify-between w-screen p-4 items-center">
+  <nav className="flex justify-between w-full p-4 items-center">
     <div className="flex items-center gap-2 font-bold">
       <div>
       <img
